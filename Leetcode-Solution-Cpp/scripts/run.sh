4c1e96c1bd15737a0657cc1a0d22806acf9d9798
combofish <combofish@139.com>
2022-05-05 15:42:04 +0800

    move *.cpp to src/*.cpp.

--- Leetcode-Solution-Cpp/scripts/run.sh
+++ Leetcode-Solution-Cpp/scripts/run.sh
@@ -8,5 +8,11 @@ filename="test_"$1
 echo "zsh| Processing $filename"
 echo ""
 
+DIR="./run"
+if [ ! -d "$DIR" ];then
+   mkdir "$DIR"
+   echo "create dir: $DIR"
+fi
+   
 # compile and run and del
-g++ --std=c++2a -g "${filename}.cpp" -o ./run/$filename && ./run/$filename && rm ./run/$filename
+g++ --std=c++2a -g "./src/${filename}.cpp" -o ./run/$filename && ./run/$filename && rm ./run/$filename
