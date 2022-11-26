22a0afd092bd417f14be8a4cde8d6691f89fcc78
combofish <combofish@139.com>
2022-11-26 19:21:44 +0800

    Add solutions of leetcode learned from programmercarl.

--- Leetcode-Solution-Cpp/src/combofish_utils.hpp
+++ Leetcode-Solution-Cpp/src/combofish_utils.hpp
@@ -27,7 +27,9 @@
 #include <unordered_set>
 #include <utility>
 #include <vector>
+#include <list>
 
+using std::list;
 using std::cout;
 using std::endl;
 using std::pair;
@@ -44,112 +46,173 @@ typedef vector<vector<vector<int>>> tensor_t;
 typedef vector<vector<int>> mat_t;
 typedef vector<int> vec_t;
 
-void out_qu(queue<int> qu_) {
-  vec_t qu;
-  while (!qu_.empty()) {
-    qu.emplace_back(qu_.front());
-    qu_.pop();
-  }
+void out_sta(stack<int> sta_) {
+    vec_t sta;
+    while (!sta_.empty()) {
+        sta.emplace_back(sta_.top());
+        sta_.pop();
+    }
 
-  IC(qu);
+    std::reverse(sta.begin(), sta.end());
+    IC(sta);
 }
+
+void out_qu(queue<int> qu_) {
+    vec_t qu;
+    while (!qu_.empty()) {
+        qu.emplace_back(qu_.front());
+        qu_.pop();
+    }
+
+    IC(qu);
+}
+
 struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode() : val(0), left(nullptr), right(nullptr) {}
-  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right)
-      : val(x), left(left), right(right) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+            : val(x), left(left), right(right) {}
+};
+
+struct MyTreeNode {
+    int val;
+    MyTreeNode *left;
+    MyTreeNode *right;
+
+    MyTreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
 // 从列表构建链表
 ListNode *vec_t2list_node(vec_t &nums) {
-  if (nums.empty())
-    return nullptr;
+    if (nums.empty())
+        return nullptr;
 
-  ListNode *head = new ListNode(nums.front());
-  ListNode *cur = head;
+    ListNode *head = new ListNode(nums.front());
+    ListNode *cur = head;
 
-  for (int i = 1; i < nums.size(); ++i) {
-    cur->next = new ListNode(nums[i]);
-    cur = cur->next;
-  }
+    for (int i = 1; i < nums.size(); ++i) {
+        cur->next = new ListNode(nums[i]);
+        cur = cur->next;
+    }
 
-  return head;
+    return head;
+}
+
+void deleteListNode(ListNode *node) {
+    while (node != nullptr) {
+        ListNode *tmp = node;
+        node = node->next;
+        delete tmp;
+    }
+}
+
+
+void deleteListNode(ListNode *input, int n) {
+    ListNode *cur = input;
+    while (n-- && cur != nullptr) {
+        ListNode *tmp = cur;
+        cur = cur->next;
+        delete tmp;
+    }
 }
 
 // 输出链表
 void out_list_node(ListNode *node, string s = "ListNode") {
-  vec_t res;
-  while (node != nullptr) {
-    res.emplace_back(node->val);
-    node = node->next;
-  }
-
-  IC(s, res);
-}
-
-template <typename T> void out_mat(vector<vector<T>> &mat, string s = "Res") {
-
-  cout << s << endl;
-  for (auto line : mat) {
-    for (auto n : line) {
-      cout << "   " << n;
+    vec_t res;
+    while (node != nullptr) {
+        res.emplace_back(node->val);
+        node = node->next;
     }
-    cout << endl;
-  }
 
-  cout << endl;
+    //    delete node;
+    IC(s, res);
 }
 
-template <typename T> void out_vec(vector<T> &vec, string s = "Res") {
-  cout << s << " :";
-  for (auto &c : vec)
-    cout << c << ", ";
-  cout << endl;
+template<typename T>
+void out_mat(vector<vector<T>> &mat, string s = "Res") {
+
+    cout << s << endl;
+    for (auto line: mat) {
+        for (auto n: line) {
+            cout << "   " << n;
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+}
+
+template<typename T>
+void out_vec(vector<T> &vec, string s = "Res") {
+    cout << s << " :";
+    for (auto &c: vec)
+        cout << c << ", ";
+    cout << endl;
 }
 
 // template <typename T> void out(T &t) { cout << t << endl; }
 // template <typename T> void outln(T &t) { cout << t; }
 
-template <typename T> void out(T t, string s = "Res") {
-  cout << s << ": " << t << " ";
+template<typename T>
+void out(T t, string s = "Res") {
+    cout << s << ": " << t << " ";
 }
-template <typename T> void outln(T t, string s = "Res") {
-  cout << s << ": " << t << endl;
+
+template<typename T>
+void outln(T t, string s = "Res") {
+    cout << s << ": " << t << endl;
 }
 
 void out_line() { cout << endl; }
 
 void posterOrder(TreeNode *root) {
-  if (root != nullptr) {
-    cout << root->val << endl;
-    posterOrder(root->left);
-    posterOrder(root->right);
-  }
+    if (root != nullptr) {
+        cout << root->val << endl;
+        posterOrder(root->left);
+        posterOrder(root->right);
+    }
 }
 
 void middleOrder(TreeNode *root) {
-  if (root != nullptr) {
-    middleOrder(root->left);
-    cout << root->val << endl;
-    middleOrder(root->right);
-  }
+    if (root != nullptr) {
+        middleOrder(root->left);
+        cout << root->val << endl;
+        middleOrder(root->right);
+    }
 }
 
 void hindOrder(TreeNode *root) {
-  if (root != nullptr) {
-    hindOrder(root->left);
-    hindOrder(root->right);
-    cout << root->val << endl;
-  }
+    if (root != nullptr) {
+        hindOrder(root->left);
+        hindOrder(root->right);
+        cout << root->val << endl;
+    }
+}
+
+/**
+ * recursive delete TreeNode*
+ * @param pNode
+ */
+void deleteTreeNode(TreeNode *pNode) {
+    if (!pNode) return;
+    deleteTreeNode(pNode->left);
+    deleteTreeNode(pNode->right);
+    delete pNode;
 }
--- Leetcode-Solution-Cpp/src/try_96.cpp
+++ Leetcode-Solution-Cpp/src/try_96.cpp
@@ -13,12 +13,19 @@ Space: O()
 #include <algorithm>
 
 class Solution {
+private:
 
     // function start.
 public:
+    int numTrees(int n) {
+        vector<int> dp(n+1);
+        dp[0] = 1;
+        for(int i = 1; i<=n; i++)
+            for(int j  = 1; j<=i;j++)
+                dp[i] += dp[j-1] * dp[i-j];
 
-
-private:
+        return dp.back();
+    }
 
     // function end.
 };
@@ -28,7 +35,10 @@ int main() {
     auto solution = new Solution();
 
     // code start
-
+    int n = 3;
+    int rel = 5;
+    IC(n, rel);
+    IC(solution->numTrees(n) == rel);
     // code end
 
     delete solution;
